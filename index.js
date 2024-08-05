fbaecf964f766bb1807eb4813a644227db685a4a
fwldom <m989mahdi@gmail.com>
2024-08-05 16:18:56 +0330

    6

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-  document.getElementById("search-submit").addEventListener("click", function() {
-          var searchInput = document.getElementById("search-input").value;
-           window.location.href = "https://www.google.com/search?q=" + searchInput;
-   });
-   document.getElementById("search-submit").addEventListener('keydown', (event) => {
+document.getElementById("search-submit").addEventListener("click", function () {
+        var searchInput = document.getElementById("search-input").value;
+        window.location.href = "https://www.google.com/search?q=" + searchInput;
+});
+document.getElementById("search-input").addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
                 document.getElementById("search-submit").click();
         }
-    });
+});
